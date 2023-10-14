216101fb99f0ef64fe2553e16fa80556a9908342
Brom Bresenham <brom.bresenham@runegate.com>
2023-10-14 13:29:26 -0400

    [v1.4.1]

--- DemoProject/Border.gd
+++
@@ -1,6 +0,0 @@
-@tool
-
-extends Control
-
-func _draw():
-	draw_rect( Rect2(position,size), Color(1,1,1), false, 1 )
--- DemoProject/LayoutConfig.gd
+++
@@ -1,8 +0,0 @@
-@tool
-
-extends GGLayoutConfig
-
-func _on_begin_layout( display_size:Vector2 ):
-	set_parameter( "half_width", int(display_size.x/2) )
-	set_parameter( "half_height", int(display_size.y/2) )
-
